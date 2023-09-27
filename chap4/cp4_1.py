58b919fc3f59e36d87b7355430a9e73051e6fce7
Hyeongyu <dg197870@gmail.com>
2023-09-27 02:17:49 +0000

    test

--- chap4/cp4_1.py
+++ chap4/cp4_1.py
@@ -2,12 +2,12 @@ import random
 print("I sense your energy. Your true emotions are coming across my screen.")
 print("Your are...")
 print("\t\t-----------\n\t\t|         |\n\t\t| O    O  |\n\t\t|   <     |\n\t\t|         |")
-mood = random.randrange(3)
+mood = random.randrange(0,2)
 if mood == 0:
     print("\t\t| .     . |\n\t\t|  '...'  |\n\t\t-----------")
-if mood == 1:
+elif mood == 1:
     print("\t\t|   ...   |\n\t\t| .'   '. |\n\t\t-----------")
-if mood == 2:
+elif mood == 2:
     print("\t\t| ....... |\n\t\t|         |\n\t\t-----------")
 else:
     print("Illegal mood vlue!")
