be27378e631ebba6b0a19a8800d1ac887179e2c0
Hyeongyu <dg197870@gmail.com>
2023-10-18 02:11:59 +0000

    incomplete

---
+++ chap7/ch7.py
@@ -0,0 +1,64 @@
+import random
+display_instruct()
+turn = X
+board = new_board()
+display_board(board)
+while not winner(board):
+    if turn == human:
+        move = human_move(board, human)
+        board[move] = human
+    else:
+        move = cmptr_mv(board,hmn,cmptr)
+        board[move] = computer
+    display_board(board)
+    turn = next_turn(turn)
+winner = winner(board)
+congrat_winner(winner,human,computer)
+def display_instruct():
+    print("\tWelcome to the greatest intellectual challenge of all time: Tic-Tac-Toe.\n\tThis will be a showdown between your human brain and my silicon procesor.\n\n\n\tYou will make our move known by entering a number, 0 - 8. The number willcorrespond to the board position as ilustrated:")
+    print("\n\t\t\t\t0 | 1 | 2\n\t\t\t\t---------\n\t\t\t\t3 | 4 | 5\n\t\t\t\t---------\n\t\t\t6 | 7 | 8\n")
+    print("\nPrepare your self, human. The ultimate batter is about to begin.\n\n\n")
+    ask_yes_no()
+def new_board():
+    grid[] = 
+def display_board(board):
+
+def winner(board):
+    human = X
+    if grid[0] == grid[1] == grid[2] or \
+       grid[3] == grid[4] == grid[5] or \
+       grid[6] == grid[7] == grid[8] or \
+       grid[0] == grid[3] == grid[6] or \
+       gird[1] == grid[4] == grid[7] or \
+       gird[2] == grid[5] == grid[8] or \
+       gird[0] == grid[4] == gird[8] or \
+       gird[2] == gird[4] == gird[6]:
+        return True
+    else:
+        return False
+def human_move(board, human):
+    
+def computer_move(board, human, computer):
+
+def next_turn(turn):
+    if turn == X:
+        turn = O
+    return turn
+def congrat_winner(winner, human, computer):
+    print("As I predicted, human, I am triumphant once more.\nProof that computers are superior to humans in all regards.")
+    print("No, no! It cannot be! Somehow you tricked me, human.\nBut never again! I, the computer, so swears it!")
+def ask_yes_no(question):
+    question = input("Do you require the first move? <y/n>: ")
+    global turn
+    if question == 'y':
+        turn = X
+    elif question == 'n':
+        turn = O
+    else:
+        question = input("Plese input again: ")
+def ask_number(question, low, high):
+    question = int(input("Where will you move? <0 - 8>:"))
+    print("Fine..")
+def legal_move(board):
+
+
